0d9e4c9dd0e1375f8bfb385e4201077fe3d4fb33
Michael Akpabey <micakpabey@gmail.com>
2025-07-20 21:28:34 -0400

    Supervisor agent initial implementation and project structure implementation

---
+++ utils/llm.py
@@ -0,0 +1,14 @@
+from langchain_core.runnables import Runnable
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+#from langchain_groq import ChatGroq
+
+load_dotenv("../.env")
+
+llm = ChatOpenAI(model="gpt-4o")
+
+def get_llm() -> Runnable:
+    return llm
+
+
+#dynamic implementation, say user wants groq over chatgpt
